9756dd81469e08c38efaf8e6f5af9efae2cac7c2
9813210961 <nsirish24@gmail.com>
2024-11-11 21:19:17 +0545

    FIrst ini

---
+++ components/footer.php
@@ -0,0 +1,57 @@
+<link rel="stylesheet" href="ms_style.css">
+<section id="contact">
+        <div class="contact container">
+            <div>
+                <h1 class="section-title">Contact <span>Us</span></h1>
+            </div>
+            <div class="contact-items">
+                <div class="contact-item">
+                    <div class="icon"><img src="images/ph.jpg" /></div>
+                    <div class="contact-info">
+                        <h1>Phone</h1>
+                        <h2>+977 9813210961</h2>
+                    </div>
+                </div>
+                <div class="contact-item">
+                    <div class="icon"><img src="images/ma.jpg" /></div>
+                    <div class="contact-info">
+                        <h1>Email</h1>
+                        <h2>nsirish@gmail.com</h2>
+                    </div>
+                </div>
+                <div class="contact-item">
+                    <div class="icon"><img src="images/add.jpg" /></div>
+                    <div class="contact-info">
+                        <h1>Address</h1>
+                        <h2>Jorpati, Kathmandu</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<footer class="footer">
+
+   <section class="grid">
+
+   <section id="footer">
+        <div class="footer container">
+            <div class="brand">
+                <h2><span>Musical Shop<span></h2>
+            </div>
+            <div class="social-icon">
+                <div class="social-item">
+                    <a href="contact.php"><img src="images/fbb.jpg" /></a>
+                </div>
+                <div class="social-item">
+                    <a href="contact.php"><img src="images/ig.jpg" /></a>
+                </div>
+                <div class="social-item">
+                    <a href="contact.php"><img src="images/tt.png" /></a>
+                </div>
+            </div>
+            <p>Copyright © 2024 Musical Shop. All rights reserved</p>
+        </div>
+    </section>
+    <!-- End Footer -->
+    <script src="./app.js"></script>    
+</footer>
